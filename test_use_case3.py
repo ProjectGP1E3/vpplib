6179229a7ce70d153350f38683e3b108426f0169
ProjectGP1E3 <147192840+ProjectGP1E3@users.noreply.github.com>
2024-01-20 21:17:13 +0100

    work to do before monday

--- test_use_case3.py
+++ test_use_case3.py
@@ -1,3 +1,82 @@
 """
 implement paper from sciebo for use case 3
 """
+
+"""
+other parameters from paper to be consider 
+"""
+
+"""
+# Parameters to be defined for CHP unit ---Aysu
+"""
+
+"""
+# Parameters to be defined for PV  ---Aysu
+"""
+
+"""
+# Parameters to be defined for Battery unit --Aysu
+"""
+
+"""
+#Parameter to be defined for Thermal storage unit (Heat tank)--Aysu
+"""
+
+"""
+#Parameter to be defined for Enviroment--Aysu
+"""
+
+"""
+#Parameter to be defined for user Profile---Aysu
+"""
+
+"""
+# initialisation of enviroment --Aysu
+"""
+
+"""
+# initialisation of user Profile--Aysu
+"""
+
+
+"""
+# initialisation of PV--Aysu
+"""
+"""
+#Define optimisation parameter---Naveen
+
+"""
+
+"""
+# Access environmental data using the Environment instance ---Naveen
+
+ Heat demand 
+
+ Residual load 
+"""
+
+"""
+#create variables   ---Naveen
+
+Heat demand 
+Residual load 
+
+"""
+"""
+
+# Defining decision variables  Naveen/Aijaz
+
+"""
+
+"""
+# Defined  constraints 10 #  Naveen/Aijaz
+"""
+"""
+# Defined 7 remaining constraints  Desmond
+
+"""
+
+"""
+# Defined objective function ---Desmond
+
+"""
