089d082099f5b1304e21c2520d40c063da4e9d49
Naveen Kumar Arjunan <148177007+NaveenKumarArjunan@users.noreply.github.com>
2024-03-06 19:56:57 +0100

    Function for plot label change

---
+++ plotDataGenerator.py
@@ -0,0 +1,10 @@
+import pandas as pd
+def plotDataGenerator(start,end,plot_time_freq,price_time_freq):
+    time_series = pd.date_range(start=start, end=end, freq=plot_time_freq)
+    price_time_series = pd.date_range(start=start, end=end, freq=price_time_freq)
+    data=data = range(1, len(time_series) + 1)
+    time_series_df = pd.DataFrame(data, index=time_series, columns=['Index'])
+
+    return time_series
+
+    
